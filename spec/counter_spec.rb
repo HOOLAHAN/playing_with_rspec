4c436454f0b7ffddf8f2cb02ff6f38132d6d0371
Iain <iain.hoolahan@gmail.com>
2022-10-11 15:21:54 +0100

    class rspec test 1 completed

--- spec/counter_spec.rb
+++ spec/counter_spec.rb
@@ -1,10 +1,17 @@
 # File: spec/counter_spec.rb
 
 require 'counter'
-describe Counter do
+RSpec.describe Counter do
   it "returns the current count" do
     counter = Counter.new()
     result = counter.add(5)
+    report = "Counted to #{result} so far."
     expect(report).to eq "Counted to 5 so far."
+  end
+  it "returns the current count" do
+    counter = Counter.new()
+    result = counter.add(10)
+    report = "Counted to #{result} so far."
+    expect(report).to eq "Counted to 10 so far."
   end
 end
