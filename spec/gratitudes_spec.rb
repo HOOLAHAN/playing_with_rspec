06f799d5393392026f5703b9a1a3bbae8b1aa5cd
Iain <iain.hoolahan@gmail.com>
2022-10-11 17:14:29 +0100

    Finished gratitudes exercise

--- spec/gratitudes_spec.rb
+++ spec/gratitudes_spec.rb
@@ -5,8 +5,14 @@ require 'gratitudes'
 RSpec.describe Gratitudes do
   it 'provides list of gratitudes with opening statement' do
     gratitudes = Gratitudes.new()
-    gratitudes = add("Makers Academy")
-    result = format(gratitudes)
+    gratitudes.add(["Makers Academy"])
+    result = gratitudes.format
     expect(result).to eq "Be grateful for: Makers Academy"
+  end
+  it 'provides list of gratitudes with opening statement' do
+    gratitudes = Gratitudes.new()
+    gratitudes.add(["Iain", "Tom", "Ruby", "Pair Programming"])
+    result = gratitudes.format
+    expect(result).to eq "Be grateful for: Iain, Tom, Ruby, Pair Programming"
   end
 end
