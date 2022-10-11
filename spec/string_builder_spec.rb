51af8d34e419531195edeae464060035b1994318
Tom Fyfe <tomfyfe@MASTUDENT-Tom-Fyfe-W7JGKXKG9H.local>
2022-10-11 15:38:21 +0100

    EX.2/classes - string_builder

--- spec/string_builder_spec.rb
+++ spec/string_builder_spec.rb
@@ -1,6 +1,11 @@
 # File: spec/string_builder_spec.rb
 
 require 'string_builder'
-  RSpec.describe StringBuilder do
-    it "returns the total length of string(s)" do
-      
+RSpec.describe StringBuilder do
+  it "returns the total length of string(s)" do
+    str = StringBuilder.new()
+    string = str.add("strings for test")
+    length = string.length
+    expect(length).to eq 16
+  end
+end 
